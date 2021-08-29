7ae853356eb8611825c2031dfbe5483170ae353c
Taggart Smith <taggartsmith@taggarts-mbp.lan>
2021-08-28 20:13:06 -0600

    Final

---
+++ src/helpers/icons.js
@@ -0,0 +1,9 @@
+import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faEnvelope, faMapMarkedAlt, faPhone, faLock } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+const Icons = () => {
+    library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faEnvelope, faMapMarkedAlt, faPhone, faLock);
+};
+
+export default Icons;
+
